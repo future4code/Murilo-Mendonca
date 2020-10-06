ed6292633b2e8304cdbbc2062d1fb9555a70f11e
murilomendonca89 <murilomendonca@live.com>
2020-10-06 16:45:40 -0300

    salvo

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,4 +1,12 @@
-function addProduto () {
-    const addInput = document.getElementById("tarefa")
-    console.log(addInput)
+function addTarefa () {
+    const tarefasUsuario = document.getElementById("tarefa")
+    const diaSemana = document.getElementById("dias-semana")
+    const listaDeTarefas = document.getElementById(diaSemana)
+    
+   if (tarefasUsuario.value === "") {
+       alert("Digite uma tarefa")
+   } else {
+       listaDeTarefas.innerHTML += `<li>${tarefasUsuario}</li>`
+       tarefasUsuario.value = ""
+   }
 }
