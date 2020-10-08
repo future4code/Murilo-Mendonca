eb7b6e0891be17c50377286b74ab2f620eede211
murilomendonca89 <murilomendonca@live.com>
2020-10-07 21:47:34 -0300

    Atualização do código com inserir imagens

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -3,16 +3,28 @@ let criarPost = () => {
   let inputAutor = document.getElementById("autor-post");
   let inputConteudo = document.getElementById("conteudo-post");
   let imprimirNoHtml = document.getElementById("container-de-posts");
+  let imgPost = document.getElementById("imagem-post");
 
   let conteudoUsuario = {
     titulo: inputTitulo.value,
     autor: inputAutor.value,
     conteudo: inputConteudo.value,
+    imagem: imgPost.value,
   };
+
+  if (imgPost.value.includes("http")) {
+    imprimirNoHtml.innerHTML += `<img class="img" src=${imgPost.value}>`;
+  } else {
+    window.alert("Envie um link válido.");
+  }
 
   imprimirNoHtml.innerHTML += `<br> ${inputTitulo.value} <br> ${inputAutor.value} <br> ${inputConteudo.value}`;
 
   inputTitulo.value = "";
   inputAutor.value = "";
   inputConteudo.value = "";
+  imgPost.value = "";
+
+  let arrayDeObejos = [conteudoUsuario];
+  console.log(arrayDeObejos);
 };
