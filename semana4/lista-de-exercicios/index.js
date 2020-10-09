f120831341327c03d1bd8d64f1de8743ec1cbe56
murilomendonca89 <murilomendonca@live.com>
2020-10-09 14:34:00 -0300

    exercicio 2 - interpretacao de codigos

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,7 @@
 
 // EXERCÍCIO 01:
 //R: O console irá retornar o valor de R$100 vezes a cotação que o usuário irá digitar.
+
+// EXERCÍCIO 02:
+//R: O console.log de novoMontante vai receber o valor de 150*1.1
+//R: O console.log de segundoMontante vai receber o valor de "Tipo de investimento informado incorreto!"
