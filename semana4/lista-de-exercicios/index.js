e29490eac1a8d6c78de7d71c1c6b4580d60bf9cf
murilomendonca89 <murilomendonca@live.com>
2020-10-09 14:33:09 -0300

    exercicio 1 interpretacao de codigos

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+// -------------------------- LISTA DE EXERCÍCIOS ---------------------
+// -------------------------- INTERPRETAÇÃO DE CODIGOS ----------------
+
+// EXERCÍCIO 01:
+//R: O console irá retornar o valor de R$100 vezes a cotação que o usuário irá digitar.
