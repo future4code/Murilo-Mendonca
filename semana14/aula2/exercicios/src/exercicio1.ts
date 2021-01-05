583fb0e32655f2bccc439c182f476a06cea8aee8
murilomendonca89 <murilomendonca@live.com>
2021-01-05 17:40:43 -0300

    exercicios aula2 - semana 15

---
+++ semana14/aula2/exercicios/src/exercicio1.ts
@@ -0,0 +1,82 @@
+// EXERCICIO 1:
+
+// A) 
+// Ao atribuir um número a uma variável string, retorna um erro dizendo que números não são aceitos.
+// const minhaString: string = 32
+
+// B)
+// Devemos declarar a variável utilizando o Union Type.
+// const meuNumero: string | number = 23
+
+// C)
+// const pessoa: { nome: string, idade: number, corFavorita: string } = {
+//     nome: "Murilo",
+//     idade: 31,
+//     corFavorita: "Preta"
+// }
+
+// D)
+
+// type pessoa = {
+//     nome: string,
+//     idade: number,
+//     corFavorita: string
+// }
+
+// const usuario1: pessoa = {
+//     nome: "Murilo",
+//     idade: 31,
+//     corFavorita: "Preta"
+// }
+
+// const usuario2: pessoa = {
+//     nome: "Joaozinho",
+//     idade: 45,
+//     corFavorita: "Azul"
+// }
+
+// const usuario3: pessoa = {
+//     nome: "Mariazinha",
+//     idade: 31,
+//     corFavorita: "Rosa"
+// }
+
+
+// E)
+// enum ArcoIris {
+//     VIOLETA = "Violeta",
+//     ANIL = "Anil",
+//     AZUL = "Azul",
+//     VERDE = "Verde",
+//     AMARELO = "Amarelo",
+//     LARANJA = "Laranja",
+//     VERMELHO = "Vermelho"
+// }
+
+// type pessoa = {
+//     nome: string,
+//     idade: number,
+//     corFavorita: ArcoIris
+// }
+
+// const usuario1: pessoa = {
+//     nome: "Murilo",
+//     idade: 31,
+//     corFavorita: ArcoIris.LARANJA
+// }
+
+// const usuario2: pessoa = {
+//     nome: "Joaozinho",
+//     idade: 45,
+//     corFavorita: ArcoIris.ANIL
+// }
+
+// const usuario3: pessoa = {
+//     nome: "Mariazinha",
+//     idade: 31,
+//     corFavorita: ArcoIris.VERMELHO
+// }
+
+
+
+
---
+++ semana14/aula2/exercicios/src/exercicio4.ts
@@ -0,0 +1,37 @@
+// EXERCICIO 4:
+
+// A)
+// Utilizando o comando: tsc nomeDoArquivo.ts
+
+// type pokemon {
+// 	name: string,
+//   types: string,
+// 	healthPoints: number
+// }
+
+// const pokemon1: pokemon = {
+//   name: "Charmander",
+//   types: "Fire",
+//   healthPoints: 28
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Bulbasaur",
+//   types: "Grass/Poison",
+//   healthPoints: 31
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Squirtle",
+//   types: "Water",
+//   healthPoints: 35
+// }
+
+// B)
+// Devemos utilizar o comando: tsc ./src/nomeDoArquivo.ts
+
+// C) 
+// Podemos entrar na pasta onde estão os arquivos ts, e rodar o comando: tsc
+
+// D)
+// As opções do slide são mais completas, porém nem todas são utilizadas nesse momento. Muitas delas não tenho conhecimento para dizer para que servem. Já as que utilizamos hoje, são as básicas para rodar o projeto.
